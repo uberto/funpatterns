cd343b8d82e4c62eb8f7f6c58eabad38afc60a83
uberto <uberto.gama@gmail.com>
2019-06-30 14:09:32 +0100

    JsonCompactor exercise

---
+++ src/main/kotlin/day1/exercise2/JsonCompactor.kt
@@ -0,0 +1,17 @@
+package day1.exercise2
+
+
+fun compactJson(json: Sequence<Char>): String =
+    json.fold( TODO(), ::compactor).newJson
+
+
+fun compactor(prev: JsonCompactor, c: Char): JsonCompactor =
+    prev.compact(c)
+
+
+
+
+sealed class JsonCompactor{
+    abstract val newJson: String
+    abstract fun compact(c: Char): JsonCompactor
+}
