d5efd7103045a05e203935136ab2301ada8261a7
asciarra <angelo.sciarra@lastminute.com>
2019-07-04 19:15:08 +0200

    soluzioni a exercise 1 e 2

--- src/main/kotlin/day1/exercise2/JsonCompactor.kt
+++ src/main/kotlin/day1/exercise2/JsonCompactor.kt
@@ -2,16 +2,36 @@ package day1.exercise2
 
 
 fun compactJson(json: Sequence<Char>): String =
-    json.fold( TODO(), ::compactor).newJson
+    json.fold(ClosedQuoteJsonCompactor(""), ::compactor).newJson
 
 
 fun compactor(prev: JsonCompactor, c: Char): JsonCompactor =
     prev.compact(c)
 
-
-
-
-sealed class JsonCompactor{
+sealed class JsonCompactor {
     abstract val newJson: String
     abstract fun compact(c: Char): JsonCompactor
+}
+
+data class OpenQuoteJsonCompactor(override val newJson: String) : JsonCompactor() {
+
+    override fun compact(c: Char): JsonCompactor =
+        when (c) {
+            '"' -> ClosedQuoteJsonCompactor(newJson + "\"")
+            else -> OpenQuoteJsonCompactor(newJson + c)
+        }
+
+}
+
+data class ClosedQuoteJsonCompactor(override val newJson: String) : JsonCompactor() {
+
+    override fun compact(c: Char): JsonCompactor =
+        when (c) {
+            '\t' -> ClosedQuoteJsonCompactor(newJson)
+            '\n' -> ClosedQuoteJsonCompactor(newJson)
+            ' ' -> ClosedQuoteJsonCompactor(newJson)
+            '"' -> OpenQuoteJsonCompactor(newJson + "\"")
+            else -> ClosedQuoteJsonCompactor(newJson + c)
+        }
+
 }
