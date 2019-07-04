acd0094fba77965e1c29eb7f919059fcd06c72c3
uberto <uberto.gama@gmail.com>
2019-07-04 11:01:20 +0100

    Added Console test

---
+++ src/main/kotlin/day1/exercise3/EchoMachine.kt
@@ -0,0 +1,3 @@
+package day1.exercise3
+
+data class EchoMachine(val writer: (String) -> Console<Unit>, val reader: () -> Console<String>)
--- src/test/kotlin/day1/exercise3/ConsoleTest.kt
+++ src/test/kotlin/day1/exercise3/ConsoleTest.kt
@@ -4,8 +4,16 @@ import org.junit.jupiter.api.Test
 
 class ConsoleTest {
 
+
+    val pseudoPrint: (String) -> Console<Unit> = TODO()
+    val pseudoReadline: () -> Console<String> = TODO()
+
     @Test
     fun `how can we test the console?`(){
+
+        val echo = EchoMachine(pseudoPrint, pseudoReadline)
+
+        TODO("finish the test with assertions")
 
     }
 
