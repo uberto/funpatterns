0098c984f7553278734633637bf8ba39adcb1342
uberto <uberto.gama@gmail.com>
2019-07-05 20:24:11 +0100

    solutions

--- src/main/kotlin/day1/exercise3/EchoMachine.kt
+++ src/main/kotlin/day1/exercise3/EchoMachine.kt
@@ -3,9 +3,11 @@ package day1.exercise3
 data class EchoMachine(
     val writer: (String) -> Console<Unit>,
     val reader: () -> Console<String>
-){
+) {
 
-    fun echo(){
-        TODO()
+    fun echo() {
+        val msg: String = reader().exec()
+
+        writer(msg).exec()
     }
 }
