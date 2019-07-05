37f3adfe5f631c275b6d68f56f92e8c579191453
uberto <uberto.gama@gmail.com>
2019-07-05 20:24:11 +0100

    solutions

--- src/main/kotlin/day1/exercise2/JsonCompactor.kt
+++ src/main/kotlin/day1/exercise2/JsonCompactor.kt
@@ -2,7 +2,7 @@ package day1.exercise2
 
 
 fun compactJson(json: Sequence<Char>): String =
-    json.fold( TODO(), ::compactor).newJson
+    json.fold(OutQuotes(""), ::compactor).jsonCompacted
 
 
 fun compactor(prev: JsonCompactor, c: Char): JsonCompactor =
@@ -12,6 +12,24 @@ fun compactor(prev: JsonCompactor, c: Char): JsonCompactor =
 
 
 sealed class JsonCompactor{
-    abstract val newJson: String
+    abstract val jsonCompacted: String
     abstract fun compact(c: Char): JsonCompactor
+}
+
+data class InQuotes(override val jsonCompacted: String): JsonCompactor() {
+    override fun compact(c: Char): JsonCompactor =
+        if (c == '"')
+            OutQuotes(jsonCompacted+c)
+        else
+            InQuotes(jsonCompacted+c)
+}
+
+data class OutQuotes(override val jsonCompacted: String): JsonCompactor() {
+    override fun compact( c: Char): JsonCompactor =
+        if ( c in arrayOf(' ', '\n', '\t'))
+            this
+        else if (c == '"')
+            InQuotes(jsonCompacted+c)
+        else
+            OutQuotes(jsonCompacted+c)
 }
--- src/test/kotlin/day1/exercise3/ConsoleTest.kt
+++ src/test/kotlin/day1/exercise3/ConsoleTest.kt
@@ -1,28 +1,39 @@
 package day1.exercise3
 
+import assertk.assertThat
+import assertk.assertions.isEqualTo
 import org.junit.jupiter.api.Test
+import java.util.*
 
 class ConsoleTest {
 
-    //implement pseudoPrint and pseudoReadline using Queue<String> instead than stdin and stdout
+    val output = ArrayDeque<String>()
+    val input = ArrayDeque<String>()
 
-    val pseudoPrint: (String) -> Console<Unit> = TODO()
-    val pseudoReadline: () -> Console<String> = TODO()
+    val pseudoPrint: (String) -> Console<Unit> = { msg -> Console { output.add(msg); Unit } }
+    val pseudoReadline: () -> Console<String> = { Console { input.remove() } }
 
     @Test
-    fun `how can we test the console?`(){
+    fun `how can we test the console?`() {
 
         val echo = EchoMachine(pseudoPrint, pseudoReadline)
 
+        input.add("Hello")
+        input.add("Functional")
+        input.add("World")
+
+        echo.echo()
+        echo.echo()
         echo.echo()
 
-        TODO("finish the test with assertions")
-
+        assertThat(output.remove()).isEqualTo("Hello")
+        assertThat(output.remove()).isEqualTo("Functional")
+        assertThat(output.remove()).isEqualTo("World")
     }
 
 
     @Test
-    fun `test a mini cmdline calculator`(){
+    fun `test a mini cmdline calculator`() {
         // giving + 1 1
         // should give 2 as result
 
