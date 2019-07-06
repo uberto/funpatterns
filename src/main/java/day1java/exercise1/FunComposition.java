3429bcc3448b5b4e768da2801679aab48064a3e5
uberto <uberto.gama@gmail.com>
2019-07-06 12:41:09 +0100

    added solutions for java translations for example1 and exercise1

--- src/main/java/day1java/exercise1/FunComposition.java
+++ src/main/java/day1java/exercise1/FunComposition.java
@@ -5,11 +5,11 @@ import java.util.function.Function;
 public class FunComposition {
 
     static <A,B,C> Function<A,C> on( Function<B,C> left, Function<A,B> right) {
-        throw new RuntimeException("TODO");
+        return (A a) -> left.apply(right.apply(a));
     }
 
     static <A,B,C> Function<A,C> andThen( Function<A,B> left, Function<B,C> right)
     {
-        throw new RuntimeException("TODO");
+        return (A a) -> right.apply(left.apply(a));
     }
 }
