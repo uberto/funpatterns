d17d951bfabf8d14f6247c056d9250427d74543b
uberto <uberto.gama@gmail.com>
2019-07-06 12:41:09 +0100

    added solutions for java translations for example1 and exercise1

--- src/main/java/day1java/example1/Currying.java
+++ src/main/java/day1java/example1/Currying.java
@@ -6,21 +6,28 @@ import java.util.function.Function;
 public class Currying {
 
     static <A, B, C> Function<A, Function<B, C>> curry(BiFunction<A, B, C> f) {
-        throw new RuntimeException("TODO");
+        //verbose syntax
+        return (A a) -> {
+            return (B b) -> {
+                return f.apply(a, b);
+            };
+        };
     }
 
 
     static <A, B, C, D> Function<A, Function<B, Function<C, D>>> curry(TriFunction<A, B, C, D> f) {
-        throw new RuntimeException("TODO");
+        //expression lambda
+        return (A a) -> (B b) -> (C c) -> f.apply(a, b, c);
     }
 
 
     static <A, B, C, D, E> Function<A, Function<B, Function<C, Function<D, E>>>> curry(QuadFunction<A, B, C, D, E> f) {
-        throw new RuntimeException("TODO");
+        return (A a) -> (B b) -> (C c) -> (D d) -> f.apply(a, b, c, d);
     }
 
     static <A, B> B at(Function<A, B> f, A a) {
-        throw new RuntimeException("TODO");
+        return f.apply(a);
     }
 
 }
+
