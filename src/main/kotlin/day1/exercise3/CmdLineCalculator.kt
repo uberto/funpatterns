adc05779272e437087f385670477b453315c4616
asciarra <angelo.sciarra@lastminute.com>
2019-07-07 12:52:02 +0200

    small refactor

--- src/main/kotlin/day1/exercise3/CmdLineCalculator.kt
+++ src/main/kotlin/day1/exercise3/CmdLineCalculator.kt
@@ -8,20 +8,23 @@ class CmdLineCalculator(
     val cin: () -> Console<String>
 ) {
     fun enter() {
-        val input = cin().exec()
-        input.split(" ")
-            .let { (operator, operand1, operand2) ->
-                Outcome.tryThis{
-                    when (operator){
-                        "+" -> operand1.toInt() + operand2.toInt()
-                        "*" -> operand1.toInt() * operand2.toInt()
-                        else -> throw UnsupportedOperationException(operator)
-                    }
-                }
-            }.fold(
-                { cout("Error: cannot process $input").exec()},
-                { v -> cout(v.toString()).exec()}
-            )
+        cin().exec().let { input ->
+            input.split(" ")
+                .let { (operator, operand1, operand2) ->
+                    Outcome.tryThis{ execOperation(operator, operand1, operand2) }
+                }.fold(
+                    { cout("Error: cannot process $input").exec()},
+                    { v -> cout(v.toString()).exec()}
+                )
+        }
+    }
+
+    private fun execOperation(operator: String, operand1: String, operand2: String): Int {
+        return when (operator) {
+            "+" -> operand1.toInt() + operand2.toInt()
+            "*" -> operand1.toInt() * operand2.toInt()
+            else -> throw UnsupportedOperationException(operator)
+        }
     }
 
 }
