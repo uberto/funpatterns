a6b87ce805def97a3032ea9137c3cb0c26889d70
Felice Pagano <paganofelice@gmail.com>
2019-07-07 11:38:57 +0200

    implement calculator exercise

--- src/test/kotlin/day1/exercise3/ConsoleTest.kt
+++ src/test/kotlin/day1/exercise3/ConsoleTest.kt
@@ -1,33 +1,50 @@
 package day1.exercise3
 
+import assertk.assertThat
+import assertk.assertions.isEmpty
+import assertk.assertions.isEqualTo
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
+import java.util.*
 
 class ConsoleTest {
 
-    val fakeSideEffect: Console<Unit> = Console ({  })
-    val pseudoPrint: (String) -> Console<Unit> = { x -> fakeSideEffect }
-    val pseudoReadline: () -> Console<String> = { Console( {"Ciao"} ) }
+    private val stdOut = ArrayDeque<String>()
+    private val stdIn = ArrayDeque<String>()
+
+    val pseudoPrint: (String) -> Console<Unit> = { msg -> Console{ stdOut.addLast(msg) } }
+    val pseudoReadline: () -> Console<String> = { Console { stdIn.poll() } }
 
     @Test
-    fun `how can we test the console?`(){
+    fun `how can we test the console?`() {
+
+        stdIn.add("Hello")
+        stdIn.add("World")
 
         val echo = EchoMachine(pseudoPrint, pseudoReadline)
-
-        val readedLine = pseudoPrint("Come ti chiami?").andThen(pseudoReadline()).exec()
+        echo.echo()
         echo.echo()
 
-        TODO("finish the test with assertions")
-
-        Assertions.assertEquals("Ciao", readedLine)
+        assertThat(stdIn).isEmpty()
+        assertThat(stdOut.first).isEqualTo("Hello")
+        assertThat(stdOut.last).isEqualTo("World")
     }
 
 
     @Test
-    fun `test a mini cmdline calculator`(){
+    fun `test a mini cmdline calculator`() {
         // giving + 1 1
         // should give 2 as result
 
+        stdIn.add("+ 1 3")
+        stdIn.add("* 2 4")
+
+        val echo = Calculator(pseudoPrint, pseudoReadline)
+
+        echo.operation()
+        assertThat(stdOut.poll()).equals("4")
+        echo.operation()
+        assertThat(stdOut.poll()).equals("8")
     }
 
 }
