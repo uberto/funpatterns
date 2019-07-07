470640efb29605168f73fce9257b7b177d74de88
asciarra <angelo.sciarra@lastminute.com>
2019-07-07 15:08:26 +0200

    completed birthday filter

--- src/main/kotlin/day1/homeExercise/Filtering.kt
+++ src/main/kotlin/day1/homeExercise/Filtering.kt
@@ -1,10 +1,23 @@
 package day1.homeExercise
 
 import java.time.LocalDate
+import java.time.Month
 
-class IsEmployeeBirthdayToday(val today: LocalDate): (Employee) -> Boolean {
+class IsEmployeeBirthdayToday(private val today: LocalDate): (Employee) -> Boolean {
 
     override fun invoke(e: Employee): Boolean =
-        e.birthDate.month == today.month && e.birthDate.dayOfMonth == today.dayOfMonth
+        when {
+            e.birthDate.`is`(Month.FEBRUARY, 29) && today.isLeapYear -> today.`is`(Month.FEBRUARY, 29)
+            e.birthDate.`is`(Month.FEBRUARY, 29)  -> today.`is`(Month.FEBRUARY, 28)
+            else -> today.isSameDateAs(e.birthDate)
+        }
+
+
+    private fun LocalDate.isSameDateAs(other: LocalDate) =
+        other.month == this.month && other.dayOfMonth == this.dayOfMonth
+
+
+    private fun LocalDate.`is`(month: Month, dayOfMonth: Int): Boolean =
+        this.month == month && this.dayOfMonth == dayOfMonth
 
 }
--- src/test/kotlin/day1/homeExercise/IsEmployeeBirthdayTodayTest.kt
+++ src/test/kotlin/day1/homeExercise/IsEmployeeBirthdayTodayTest.kt
@@ -34,4 +34,56 @@ class IsEmployeeBirthdayTodayTest {
     }
 
 
+    @Test
+    fun `employee born on 29th February and today is the 29th of February`() {
+        val today = LocalDate.of(2020, 2, 29)
+        val employee = Employee(
+            "Doe",
+            "John",
+            LocalDate.of(1980, 2, 29),
+            Email("john@email.com")
+        )
+
+        assertTrue(IsEmployeeBirthdayToday(today)(employee))
+    }
+
+    @Test
+    fun `employee born on 29th February and today is the 28th of February of a non leap year`() {
+        val today = LocalDate.of(2019, 2, 28)
+        val employee = Employee(
+            "Doe",
+            "John",
+            LocalDate.of(1980, 2, 29),
+            Email("john@email.com")
+        )
+
+        assertTrue(IsEmployeeBirthdayToday(today)(employee))
+    }
+
+    @Test
+    fun `employee born on 29th February and today is the 28th of February of leap year`() {
+        val today = LocalDate.of(2020, 2, 28)
+        val employee = Employee(
+            "Doe",
+            "John",
+            LocalDate.of(1980, 2, 29),
+            Email("john@email.com")
+        )
+
+        assertFalse(IsEmployeeBirthdayToday(today)(employee))
+    }
+
+    @Test
+    fun `employee born on 29th February and today is not his birthday`() {
+        val today = LocalDate.of(2020, 1, 1)
+        val employee = Employee(
+            "Doe",
+            "John",
+            LocalDate.of(1980, 2, 29),
+            Email("john@email.com")
+        )
+
+        assertFalse(IsEmployeeBirthdayToday(today)(employee))
+    }
+
 }
