a353aa669a67748493b815d2f0016587e2f9ce62
asciarra <angelo.sciarra@lastminute.com>
2019-07-09 10:23:08 +0200

    small refactor of CmdLineCalculator

--- src/main/kotlin/day1/exercise3/CmdLineCalculator.kt
+++ src/main/kotlin/day1/exercise3/CmdLineCalculator.kt
@@ -4,28 +4,27 @@ import day1.example3.Outcome
 import java.lang.UnsupportedOperationException
 
 class CmdLineCalculator(
-    val cout: (String) -> Console<Unit>,
-    val cin: () -> Console<String>
+    private val cout: (String) -> Console<Unit>,
+    private val cin: () -> Console<String>
 ) {
     fun enter() {
         cin().flatMap { input: String ->
             input.split(" ")
                 .let { (operator, operand1, operand2) ->
-                    Outcome.tryThis{ execOperation(operator, operand1, operand2) }
+                    Outcome.tryThis { execOperation(operator, operand1, operand2) }
                 }.fold(
-                    { cout("Error: cannot process $input")},
-                    { v -> cout(v.toString())}
+                    { cout("Error: cannot process $input") },
+                    { v -> cout(v.toString()) }
                 )
         }.exec()
     }
 
-    private fun execOperation(operator: String, operand1: String, operand2: String): Int {
-        return when (operator) {
+    private fun execOperation(operator: String, operand1: String, operand2: String): Int =
+        when (operator) {
             "+" -> operand1.toInt() + operand2.toInt()
             "*" -> operand1.toInt() * operand2.toInt()
             else -> throw UnsupportedOperationException(operator)
         }
-    }
 
 }
 
