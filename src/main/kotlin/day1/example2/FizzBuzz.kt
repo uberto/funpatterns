ccbc69b8479bae079f05bf9244eab30a49e733bf
Matteo Baglini <matteo.baglini@gmail.com>
2019-07-10 19:23:20 +0200

    implement fizzbuzz (example2)

--- src/main/kotlin/day1/example2/FizzBuzz.kt
+++ src/main/kotlin/day1/example2/FizzBuzz.kt
@@ -1,4 +1,18 @@
 package day1.example2
 
+fun fizz(x: Int): String? = "Fizz".takeIf { x % 3 == 0 }
+fun buzz(x: Int): String? = "Buzz".takeIf { x % 5 == 0 }
 
-fun fizzBuzz(x: Int): String = TODO()
+fun ((Int) -> String?).combine(other: (Int) -> String?): (Int) -> String? = { n ->
+    when {
+        this(n) == null -> other(n)
+        other(n) == null -> this(n)
+        else -> this(n) + other(n)
+    }
+}
+
+fun ((Int) -> String?).orElse(f: (Int) -> String): (Int) -> String = { n ->
+    this(n) ?: f(n)
+}
+
+val fizzBuzz = ::fizz.combine(::buzz).orElse { it.toString() }
