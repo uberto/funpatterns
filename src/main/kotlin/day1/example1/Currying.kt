b7ccc7bc0d06d92b853423fc60c4dbd7f42d6b5f
Matteo Baglini <matteo.baglini@gmail.com>
2019-07-10 18:50:50 +0200

    implement currying (example1)

--- src/main/kotlin/day1/example1/Currying.kt
+++ src/main/kotlin/day1/example1/Currying.kt
@@ -1,12 +1,30 @@
 package day1.example1
 
-fun <A, B, C> ((A, B) -> C).curry(): (A) -> (B) -> C = TODO()
+fun <A, B, C> ((A, B) -> C).curry(): (A) -> (B) -> C = { a ->
+    { b ->
+        this(a, b)
+    }
+}
 
-fun <A, B, C, D> ((A, B, C) -> D).curry(): (A) -> (B) -> (C) -> D = TODO()
+fun <A, B, C, D> ((A, B, C) -> D).curry(): (A) -> (B) -> (C) -> D = { a ->
+    { b ->
+        { c ->
+            this(a, b, c)
+        }
+    }
+}
 
-fun <A, B, C, D, E> ((A, B, C, D) -> E).curry(): (A) -> (B) -> (C) -> (D) -> E = TODO()
+fun <A, B, C, D, E> ((A, B, C, D) -> E).curry(): (A) -> (B) -> (C) -> (D) -> E = { a ->
+    { b ->
+        { c ->
+            { d ->
+                this(a, b, c, d)
+            }
+        }
+    }
+}
 
 
-infix fun <A, B> ((A) -> B).`@`(a: A): B = TODO()
+infix fun <A, B> ((A) -> B).`@`(a: A): B = this(a)
 
 
