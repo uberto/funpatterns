0a87568f79450640c2b4af3d0aba74a1a64380cf
Matteo Baglini <matteo.baglini@gmail.com>
2019-07-10 19:52:29 +0200

    implement json compactor (exercise2)

--- src/main/kotlin/day1/exercise2/JsonCompactor.kt
+++ src/main/kotlin/day1/exercise2/JsonCompactor.kt
@@ -1,15 +1,35 @@
 package day1.exercise2
 
-
 fun compactJson(json: Sequence<Char>): String =
-    json.fold(TODO(), ::compactor).newJson
-
+    json.fold(OutOfQuote(""), ::compactor).newJson
 
 fun compactor(prev: JsonCompactor, c: Char): JsonCompactor =
     prev.compact(c)
 
+fun String.append(other: Char): String = this + other
 
 sealed class JsonCompactor {
+    val chars = listOf(' ', '\n', '\t')
+    val quote = '"'
+
     abstract val newJson: String
     abstract fun compact(c: Char): JsonCompactor
+}
+
+data class OutOfQuote(override val newJson: String) : JsonCompactor() {
+
+    override fun compact(c: Char): JsonCompactor =
+        when {
+            c == quote -> InQuote(newJson.append(c))
+            chars.contains(c) -> this
+            else -> OutOfQuote(newJson.append(c))
+        }
+}
+
+data class InQuote(override val newJson: String) : JsonCompactor() {
+
+
+    override fun compact(c: Char): JsonCompactor =
+        if (c == quote) OutOfQuote(newJson.append(c))
+        else InQuote(newJson.append(c))
 }
