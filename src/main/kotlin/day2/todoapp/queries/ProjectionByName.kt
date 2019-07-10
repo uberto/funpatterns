038728e38af0993934c2ff66f17492bdaec97c66
uberto <uberto.gama@gmail.com>
2019-07-10 23:34:24 +0100

    clean up projections

--- src/main/kotlin/day2/todoapp/queries/ProjectionByName.kt
+++ src/main/kotlin/day2/todoapp/queries/ProjectionByName.kt
@@ -4,36 +4,29 @@ import day2.todoapp.errors.TodoError
 import day2.todoapp.events.*
 import day2.todoapp.fp.Outcome
 import day2.todoapp.fp.exhaustive
-import java.util.concurrent.atomic.AtomicLong
 
-class ProjectionByName(val projector: Projector<NameRow, String>, val eventStore: EventStore<ToDoEvent, ItemId, ToDoItem>) : Projection<NameRow, ToDoEvent> {
+class ProjectionByName(
+    projector: Projector<NameRow, String>,
+    eventStore: EventStore<ToDoEvent, ItemId, ToDoItem>
+) {
 
-    val lastEventId = AtomicLong(NO_EVENT)
-
-
-    override fun updateFrom(orderedEvent: OrderedEvent<ToDoEvent>) {
-
-        if (lastEventId.get() < orderedEvent.id) {
+    fun updateFrom(orderedEvent: OrderedEvent<ToDoEvent>, projector: Projector<NameRow, String>) {
+        with(projector) {
             when (orderedEvent.event) {
-                is ToDoItemAdded -> projector.insert(
+                is ToDoItemAdded -> insert(
                     NameRow(
                         key = orderedEvent.event.name,
                         itemId = orderedEvent.event.itemId
                     )
                 )
-              else -> Unit //nothing to do
+                else -> Unit //nothing to do
             }.exhaustive
-            lastEventId.set(orderedEvent.id)
         }
-
     }
 
-    fun updatedProjector(): Projector<NameRow, String> {
-        eventStore.fetchAll(lastEventId.get()).forEach { updateFrom(it) }
-        return projector
-    }
+    val baseProjection = BaseProjection(projector, eventStore, ::updateFrom)
 
-    fun getItem(name: String): Outcome<TodoError, List<NameRow>> = updatedProjector().getFiltered { it.key == name }
+    fun getItem(name: String): Outcome<TodoError, List<NameRow>> = baseProjection.updatedProjector().getFiltered { it.key == name }
 
 
 }
